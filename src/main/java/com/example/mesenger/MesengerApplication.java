267a3a6981ea0f82eb4623695544e29638a2134a
reywoo276 <sadivskijk@gmail.com>
2025-04-16 22:46:40 +0300

    create project

---
+++ src/main/java/com/example/mesenger/MesengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesengerApplication.class, args);
+    }
+
+}
